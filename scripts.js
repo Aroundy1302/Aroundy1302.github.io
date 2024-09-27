02ebae6191587a17cd2efa0ba19e8090dd075380
Aroundy1302 <s6501075516115@email.kmutnb.ac.th>
2024-09-27 23:09:38 +0700

    Big update

--- scripts.js
+++ scripts.js
@@ -14,3 +14,16 @@ const navList = document.querySelector('.nav-list');
 navToggle.addEventListener('click', () => {
     navList.classList.toggle('active'); // Toggle the 'active' class
 });
+
+function updateData() {
+    fetch('https://206.189.146.138/api/sensors')
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('timestamp').textContent = data.timestamp;
+      document.getElementById('temperature').textContent = data.temperature.toFixed(2);
+      document.getElementById('humidity').textContent = data.humidity.toFixed(2);
+      document.getElementById('luxsensor').textContent = data.luxsensor.toFixed(2);
+      document.getElementById('motion').textContent = data.motion;
+    });
+  }
+  setInterval(updateData, 1000);
